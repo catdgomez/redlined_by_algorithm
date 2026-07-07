91e6544c6cb769dd6bba7debea55d18a2b18391d
unknown <catdgomez@gmail.com>
2026-07-07 15:25:10 -0500

    include anticipated necessary dependencies

--- app/redlined_by_algorithm.py
+++ app/redlined_by_algorithm.py
@@ -3,3 +3,12 @@ Redlined by Algorithm: Interactive Fair Lending Analysis
 Home Mortgage Disclosure Act (HMDA) Mortgage Data from Atlanta, Georgia
 """
 
+# ── Dependencies ───────────────────────────────────────────────────────────────
+import streamlit as st
+import pandas as pd
+import numpy as np
+import statsmodels.formula.api as smf
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
